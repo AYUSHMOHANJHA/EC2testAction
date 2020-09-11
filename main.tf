0ae23308835d3af5e19fa979046ec0437d91174d
Ayush Mohan Jha <AYUSHJHA06@GMAIL.COM>
2020-09-11 08:44:09 +0530

    backend remote,terraform block added

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+ backend "remote" {
+   organization = "ayu"
+
+   workspaces {
+     name = "EC2testAction"
+   }
+ }
+}
 resource "aws_instance" "example" {
     ami = "ami-058043b9f3c10c078"
     instance_type = "t2.micro"
